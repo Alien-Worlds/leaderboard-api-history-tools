fa25425b2daf33b5d65c3e0a913bc5b4cb43bde7
rkamysz <radoslaw.kamysz@gmail.com>
2023-07-11 11:03:56 +0200

    remove unnecessary processors, update deps

--- src/processor/processor.bindings.ts
+++ src/processor/processor.bindings.ts
@@ -1,15 +1,9 @@
 import { WorkerContainer } from '@alien-worlds/api-history-tools';
 import { ProcessorLabel } from './processor.labels';
-import UsptsWorldsActionProcessor from './processors/uspts-worlds/uspts-worlds.action-processor';
 import NotifyWorldActionProcessor from './processors/notify-world/notify-world.action-processor';
-import FederationActionProcessor from './processors/federation/federation-world.action-processor';
 
 const container = new WorkerContainer();
 
-// 'uspts.worlds'
-container.bind(ProcessorLabel.UsptsWorldsActionProcessor, UsptsWorldsActionProcessor);
-// 'federation'
-container.bind(ProcessorLabel.FederationActionProcessor, FederationActionProcessor);
 // 'notify.world'
 container.bind(ProcessorLabel.NotifyWorldActionProcessor, NotifyWorldActionProcessor);
 
--- src/processor/processor.worker-loader.ts
+++ src/processor/processor.worker-loader.ts
@@ -7,13 +7,10 @@ import {
 import { ProcessorSharedData } from './processor.types';
 import { ProcessorLabel } from './processor.labels';
 import NotifyWorldActionProcessor from './processors/notify-world/notify-world.action-processor';
-import UsptsWorldsActionProcessor from './processors/uspts-worlds/uspts-worlds.action-processor';
-import FederationActionProcessor from './processors/federation/federation-world.action-processor';
 import {
   LeaderboardUpdateMongoSource,
   LeaderboardUpdateRepository,
   LeaderboardUpdateRepositoryImpl,
-  setupLeaderboard,
 } from '@alien-worlds/leaderboard-api-common';
 import { setupAtomicAssets } from '@alien-worlds/atomicassets-api-common';
 
@@ -24,13 +21,10 @@ export default class ProcessorWorkerLoader extends DefaultWorkerLoader {
   public async setup(sharedData: ProcessorSharedData): Promise<void> {
     super.setup(sharedData);
     const {
-      config: { mongo, leaderboard, atomicassets },
+      config: { mongo, atomicassets },
     } = sharedData;
     this.ioc = new Container();
-    const [mongoSource, leaderboardApiMongoSource] = await Promise.all([
-      MongoSource.create(mongo),
-      MongoSource.create(leaderboard.mongo),
-    ]);
+    const mongoSource = await MongoSource.create(mongo);
 
     this.ioc
       .bind<LeaderboardUpdateRepository>(LeaderboardUpdateRepository.Token)
@@ -39,18 +33,7 @@ export default class ProcessorWorkerLoader extends DefaultWorkerLoader {
       );
 
     await setupAtomicAssets(atomicassets, mongoSource, this.ioc);
-    await setupLeaderboard(leaderboard, leaderboardApiMongoSource, this.ioc);
 
-    // 'uspts.worlds'
-    this.container.bind(
-      ProcessorLabel.UsptsWorldsActionProcessor,
-      UsptsWorldsActionProcessor
-    );
-    // 'federation'
-    this.container.bind(
-      ProcessorLabel.FederationActionProcessor,
-      FederationActionProcessor
-    );
     // 'notify.world'
     this.container.bind(
       ProcessorLabel.NotifyWorldActionProcessor,
